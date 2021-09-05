7d4745f691d784802469228a86898ffc2fc4760c
shre-yas <86412150+shre-yas@users.noreply.github.com>
2021-09-05 18:35:21 +0530

    Initial Commit

---
+++ src/wrapperClass/wrapperClass.java
@@ -0,0 +1,15 @@
+package wrapperClass;
+
+public class wrapperClass {
+
+	public static void main(String[] args) {
+
+		Integer i = 100;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(i.longValue());
+		System.out.println(i.hashCode());
+
+	}
+
+}
